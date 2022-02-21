587857a065dbf401fb47c817ffc2506eebbddcb2
wu-kan <i@wu-kan.cn>
2022-02-21 22:46:15 +0800

    update sylib

--- sylib/sylib.cc
+++ sylib/sylib.cc
@@ -2,6 +2,39 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <sys/time.h>
+
+/* Timing function implementation */
+static struct timeval _sysy_start, _sysy_end;
+static int _sysy_l1[_SYSY_N], _sysy_l2[_SYSY_N];
+static int _sysy_h[_SYSY_N], _sysy_m[_SYSY_N], _sysy_s[_SYSY_N],
+    _sysy_us[_SYSY_N];
+static int _sysy_idx;
+
+static __attribute((constructor)) void before_main() {
+  for (int i = 0; i < _SYSY_N; i++)
+    _sysy_h[i] = _sysy_m[i] = _sysy_s[i] = _sysy_us[i] = 0;
+  _sysy_idx = 1;
+}
+static __attribute((destructor)) void after_main() {
+  for (int i = 1; i < _sysy_idx; i++) {
+    fprintf(stderr, "Timer@%04d-%04d: %dH-%dM-%dS-%dus\n", _sysy_l1[i],
+            _sysy_l2[i], _sysy_h[i], _sysy_m[i], _sysy_s[i], _sysy_us[i]);
+    _sysy_us[0] += _sysy_us[i];
+    _sysy_s[0] += _sysy_s[i];
+    _sysy_us[0] %= 1000000;
+    _sysy_m[0] += _sysy_m[i];
+    _sysy_s[0] %= 60;
+    _sysy_h[0] += _sysy_h[i];
+    _sysy_m[0] %= 60;
+  }
+  fprintf(stderr, "TOTAL: %dH-%dM-%dS-%dus\n", _sysy_h[0], _sysy_m[0],
+          _sysy_s[0], _sysy_us[0]);
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Input & output functions */
 int getint() {
   int t;
@@ -29,40 +62,11 @@ void putarray(int n, int a[]) {
   printf("\n");
 }
 
-/* Timing function implementation */
-static struct timeval _sysy_start, _sysy_end;
-#define _SYSY_N 1024
-static int _sysy_l1[_SYSY_N], _sysy_l2[_SYSY_N];
-static int _sysy_h[_SYSY_N], _sysy_m[_SYSY_N], _sysy_s[_SYSY_N],
-    _sysy_us[_SYSY_N];
-static int _sysy_idx;
-
-/* Timing function implementation */
-static __attribute((constructor)) void before_main() {
-  for (int i = 0; i < _SYSY_N; i++)
-    _sysy_h[i] = _sysy_m[i] = _sysy_s[i] = _sysy_us[i] = 0;
-  _sysy_idx = 1;
-}
-static __attribute((destructor)) void after_main() {
-  for (int i = 1; i < _sysy_idx; i++) {
-    fprintf(stderr, "Timer@%04d-%04d: %dH-%dM-%dS-%dus\n", _sysy_l1[i],
-            _sysy_l2[i], _sysy_h[i], _sysy_m[i], _sysy_s[i], _sysy_us[i]);
-    _sysy_us[0] += _sysy_us[i];
-    _sysy_s[0] += _sysy_s[i];
-    _sysy_us[0] %= 1000000;
-    _sysy_m[0] += _sysy_m[i];
-    _sysy_s[0] %= 60;
-    _sysy_h[0] += _sysy_h[i];
-    _sysy_m[0] %= 60;
-  }
-  fprintf(stderr, "TOTAL: %dH-%dM-%dS-%dus\n", _sysy_h[0], _sysy_m[0],
-          _sysy_s[0], _sysy_us[0]);
-}
-static void _sysy_starttime(int lineno) {
+void _sysy_starttime(int lineno) {
   _sysy_l1[_sysy_idx] = lineno;
   gettimeofday(&_sysy_start, NULL);
 }
-static void _sysy_stoptime(int lineno) {
+void _sysy_stoptime(int lineno) {
   gettimeofday(&_sysy_end, NULL);
   _sysy_l2[_sysy_idx] = lineno;
   _sysy_us[_sysy_idx] += 1000000 * (_sysy_end.tv_sec - _sysy_start.tv_sec) +
@@ -76,5 +80,6 @@ static void _sysy_stoptime(int lineno) {
   _sysy_idx++;
 }
 
-void starttime() { _sysy_starttime(_sysy_idx); }
-void stoptime() { _sysy_stoptime(_sysy_idx); }
+#ifdef __cplusplus
+}
+#endif
